8417642ae81fe9a4c205a40657cf8bfc25d5c75d
Daniel <dlfraga@github>
2025-03-21 17:51:02 -0300

    feat: add automatic startup configuration for nhsupsclient service

--- Unraid-plugin-nhs-client/nhs-ups-client/nhsupsclient/install/doinst.sh
+++ Unraid-plugin-nhs-client/nhs-ups-client/nhsupsclient/install/doinst.sh
@@ -13,3 +13,17 @@ if ! grep "$RC_SCRIPT" $SD_RCFILE >/dev/null 2>&1
 	then echo -e "\n[ -x $RC_SCRIPT ] && $RC_SCRIPT stop" >> $SD_RCFILE
 fi
 [ ! -x $SD_RCFILE ] && chmod u+x $SD_RCFILE
+
+# Make the init script executable
+chmod 755 etc/rc.d/rc.nhsupsclient
+
+# Create symlink for automatic startup
+( cd etc/rc.d/rc0.d ; rm -rf K00nhsupsclient )
+( cd etc/rc.d/rc0.d ; ln -sf ../rc.nhsupsclient K00nhsupsclient )
+( cd etc/rc.d/rc6.d ; rm -rf K00nhsupsclient )
+( cd etc/rc.d/rc6.d ; ln -sf ../rc.nhsupsclient K00nhsupsclient )
+( cd etc/rc.d/rcS.d ; rm -rf S99nhsupsclient )
+( cd etc/rc.d/rcS.d ; ln -sf ../rc.nhsupsclient S99nhsupsclient )
+
+# Start the service immediately after installation
+/etc/rc.d/rc.nhsupsclient start
